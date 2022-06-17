4abf5d162b5d4fab14fd7943eaaab850ad3c0338
Sebastian Delgado <sebastian.d.alex@gmail.com>
2022-06-17 15:48:44 -0500

    Ajuste:Cambio de estilos css

--- ch14prueba/saludo/script.js
+++ ch14prueba/saludo/script.js
@@ -7,12 +7,12 @@ function cambiarSaludo() {
   //console.log("Me parece una falta de respecto");
   let frase = document.getElementsByTagName("h3")[0];
 
-  if (frase.innerText == "Ganando como siempre") {
+  if (frase.innerText == "¡Ganando como siempre!") {
     //frase = "Impongo moda para las niñas!";
-    frase.innerText= "Impongo moda para las niñas!";
+    frase.innerText= "¡Impongo moda para las niñas!";
   } else {
     //frase = "Ganando como siempre";
-    frase.innerText = "Ganando como siempre";
+    frase.innerText = "¡Ganando como siempre!";
   }
   console.log("frase :" + frase);
 }
